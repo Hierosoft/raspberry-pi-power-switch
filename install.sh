c01dc9d713e94f579bd629b88f8686ae1e1ebf6e
Poikilos <7557867+poikilos@users.noreply.github.com>
2025-12-05 01:31:19 -0500

    Add installer.

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+INSTALL_SRC=data
+if [ ! -d "$INSTALL_SRC/etc" ]; then
+    echo "Error: You must be in the same directory as the '$INSTALL_SRC' folder."
+    exit 1
+fi
+
+
+if [ ! -d ".venv" ]; then
+    sudo apt-get update
+    echo "Installing precompiled Python packages (saves time if your distro has a version in the version range required by escpos or its dependencies)..."
+    sudo apt-get install -y python3-rpi.gpio || exit $?
+    echo "Creating .venv..."
+    python -m venv .venv
+fi
+sed -i 's|include-system-site-packages = false|include-system-site-packages = true|g' .venv/pyvenv.cfg || exit $?
+
+# works fine even though destination has slash:
+# sudo rsync -rtv $INSTALL_SRC/ / || exit $?
+cp $INSTALL_SRC/etc/systemd/system/shutdown-button.service /tmp/shutdown-button.service.tmp || exit $?
+sed -i "s|/home/user|$HOME|g" /tmp/shutdown-button.service.tmp || exit $?
+sudo cp /tmp/shutdown-button.service.tmp /etc/systemd/system/ || exit $?
+
+sudo systemctl daemon-reload
+sudo systemctl enable shutdown-button.service || exit $?
+sudo systemctl start shutdown-button.service || exit $?
